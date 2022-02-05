42566518c6b591126222b6231b6f5b08d28019de
papa-react <makiya112545@gmail.com>
2022-02-05 07:25:59 -1200

    fetch app in all way

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,35 @@
 import CreatePost from "@components/CreatePost";
+import Loader from "@components/Loader";
 import PostCard from "@components/PostCard";
+import { IPost } from "@libs/types";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const getPosts = () => {
-    
+  
+  const [posts,setPosts] = useState<IPost[]>(null)
+
+  const getPosts = async () => {
+    const {data}=await axios("/posts?_sort=createdAt&_order=desc")
+    setPosts(data)
   };
+
+  useEffect(()=>{
+    getPosts();
+  },[])
+
 
   return (
     <div>
       <h4>useSWR Hook ⛳</h4>
       <CreatePost />
-
+      
+      {/* is I dot have post */}
+      { !posts && <Loader/> }
+      
       <h4>Posts</h4>
-      {[...new Array(10)].map((_, i) => (
-        <PostCard key={i} />
+      {posts?.map((post) => (
+        <PostCard key={post.id} data={post} />
       ))}
     </div>
   );
--- src/pages/posts/[postId].tsx
+++ src/pages/posts/[postId].tsx
@@ -1,20 +1,45 @@
 import CreateComment from "@components/CreateComment";
 import CommentCard from "@components/CommentCard";
 import PostCard from "@components/PostCard";
+import { useEffect, useState } from "react";
+import { IComment, IPost } from "@libs/types";
+import axios from "axios";
+import { useRouter } from "next/router";
+import Loader from "@components/Loader";
 
 const index = () => {
-  const getPost = () => {};
+  
+  const {query:{postId}}=useRouter()
+  const [post,setPost] = useState<IPost>(null)
+  const [comments,setComments] = useState<IComment[]>(null)
 
-  const getComments = () => {};
+  const getPost = async() => {
+    const {data}=await axios(`/posts/${postId}/?_sort=createdAt&_order=desc`)
+    setPost(data)
+  };
+
+  const getComments = async() => {
+    const {data}=await axios(`/posts/${postId}/comments?_sort=createdAt&_order=desc`)
+    setComments(data);
+  };
+
+  useEffect(()=>{
+    postId && getPost();
+    postId && getComments();
+  },[postId])
 
   return (
     <div>
-      <PostCard />
+      {!post&&<Loader/>}
+      {
+        post &&  <PostCard data={post} />
+      }
       <CreateComment />
 
+      {!comments&&<Loader/>}
       <h4>Comments</h4>
-      {[...new Array(5)].map((_, i) => (
-        <CommentCard key={i} />
+      {comments?.map((comment) => (
+        <CommentCard key={comment.id} data={comment} />
       ))}
     </div>
   );
