ab9c53277a1607bd1c97551ab5e238107e4bc9e6
papa-react <makiya112545@gmail.com>
2022-02-06 03:50:55 -1200

    add fetcher in global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
 import "@styles/globals.css";
+import { SWRConfig } from "swr";
 
 axios.defaults.baseURL = "http://localhost:3001";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="p-3">
-      <Component {...pageProps} />
+      <SWRConfig value={{ fetcher:(url:string) => axios(url).then((response) => response.data),dedupingInterval:10000}} >
+        <Component {...pageProps} />
+      </SWRConfig>
     </div>
   );
 }
