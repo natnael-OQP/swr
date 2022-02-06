95921f59f48d7881b276fbdcf9e821d357e3fb80
papa-react <makiya112545@gmail.com>
2022-02-06 03:52:06 -1200

    convert in to swr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,29 +4,32 @@ import PostCard from "@components/PostCard";
 import { IPost } from "@libs/types";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 export default function Home() {
-  
-  const [posts,setPosts] = useState<IPost[]>(null)
+// =================  traditional data fetching =====================
+  // const [posts,setPosts] = useState<IPost[]>(null)
 
-  const getPosts = async () => {
-    const {data}=await axios("/posts?_sort=createdAt&_order=desc")
-    setPosts(data)
-  };
+  // const getPosts = async () => {
+  //   const {data}=await axios("/posts?_sort=createdAt&_order=desc")
+  //   setPosts(data)
+  // };
 
-  useEffect(()=>{
-    getPosts();
-  },[])
-
-
+  // useEffect(()=>{
+  //   getPosts();
+  // },[])
+// =================  SWR data fetching =====================
+  const {data:posts,error} = useSWR<IPost[]>("/posts?_sort=createdAt&_order=desc",);
+  // { refreshInterval: 10000 }
   return (
     <div>
       <h4>useSWR Hook ⛳</h4>
-      <CreatePost setPosts={setPosts} />
+      <CreatePost  />
       
+      {/* if Error occur */}
+      {error && <p>something is wrong</p> }
       {/* is I dot have post */}
       { !posts && <Loader/> }
-      
       <h4>Posts</h4>
       {posts?.map((post) => (
         <PostCard key={post.id} data={post} />
