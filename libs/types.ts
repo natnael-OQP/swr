d08bb90735844d8361ee37b131caf5f67bad583a
papa-react <makiya112545@gmail.com>
2022-02-06 19:01:07 -1200

    add clientonly type

--- libs/types.ts
+++ libs/types.ts
@@ -2,10 +2,12 @@ export interface IPost {
   content: string;
   id: number;
   createdAt:Date
+  clientOnly:boolean;
 }
 export interface IComment {
   content: string;
   id: number;
   postId: number;
   createdAt:Date
+  clientOnly:boolean;
 }
