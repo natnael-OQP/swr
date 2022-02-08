cd0957e4fa6e8a4c30ef0df874194b74dff35b5a
papa-react <makiya112545@gmail.com>
2022-02-07 20:32:37 -1200

    remove  data function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,43 +7,44 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-
 export default function Home() {
-// =================  traditional data fetching =====================
-  // const [posts,setPosts] = useState<IPost[]>(null)
+	// =================  traditional data fetching =====================
+	// const [posts,setPosts] = useState<IPost[]>(null)
 
-  // const getPosts = async () => {
-  //   const {data}=await axios("/posts?_sort=createdAt&_order=desc")
-  //   setPosts(data)
-  // };
+	// const getPosts = async () => {
+	//   const {data}=await axios("/posts?_sort=createdAt&_order=desc")
+	//   setPosts(data)
+	// };
 
-  // useEffect(()=>{
-  //   getPosts();
-  // },[])
-// =================  SWR data fetching =====================
-  // const {data:posts,error,mutate} = useSWR<IPost[]>("/posts?_sort=createdAt&_order=desc",);
-  // { refreshInterval: 10000 }
- 
-  const {data:posts,error,mutate,size,setSize}=usePagination("/posts?_sort=createdAt&_order=desc")
+	// useEffect(()=>{
+	//   getPosts();
+	// },[])
+	// =================  SWR data fetching =====================
+	// const {data:posts,error,mutate} = useSWR<IPost[]>("/posts?_sort=createdAt&_order=desc",);
+	// { refreshInterval: 10000 }
 
-  return (
-    <div>
-      <h4>useSWR Hook ⛳</h4>
-      <CreatePost mutate={mutate}  />
-      
-      {/* if Error occur */}
-      {error && <p>something is wrong</p> }
-      {/* is I dot have post */}
-      { !posts && <Loader/> }
-      <h4>Posts</h4>
-      {posts?.flat().map((post) => (
-        <PostCard key={post.id} data={post} />
-      ))}
-      <button onClick={()=>setSize(size+1)} >load more</button>
-    </div>
-  );
+	const {
+		data: posts,
+		error,
+		mutate,
+		size,
+		setSize,
+	} = usePagination("/posts?_sort=createdAt&_order=desc");
+
+	return (
+		<div>
+			<h4>useSWR Hook ⛳</h4>
+			<CreatePost mutate={mutate} />
+
+			{/* if Error occur */}
+			{error && <p>something is wrong</p>}
+			{/* is I dot have post */}
+			{!posts && <Loader />}
+			<h4>Posts</h4>
+			{posts?.flat().map((post) => (
+				<PostCard key={post.id} data={post} />
+			))}
+			<button onClick={() => setSize(size + 1)}>load more</button>
+		</div>
+	);
 }
-function data(data: any) {
-  throw new Error("Function not implemented.");
-}
-
