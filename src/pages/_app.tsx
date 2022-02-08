22ff68ad795f57f5624ea553d9371bc4d8722827
papa-react <makiya112545@gmail.com>
2022-02-07 20:33:40 -1200

    style in readabell way

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,19 @@ import { SWRConfig } from "swr";
 axios.defaults.baseURL = "http://localhost:3001";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className="p-3">
-      <SWRConfig value={{ fetcher:(url:string) => axios(url).then((response) => response.data),dedupingInterval:10000}} >
-        <Component {...pageProps} />
-      </SWRConfig>
-    </div>
-  );
+	return (
+		<div className="p-3">
+			<SWRConfig
+				value={{
+					fetcher: (url: string) =>
+						axios(url).then((response) => response.data),
+					dedupingInterval: 10000,
+				}}
+			>
+				<Component {...pageProps} />
+			</SWRConfig>
+		</div>
+	);
 }
 
 export default MyApp;
